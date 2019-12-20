238b6ae1e979d2d85e17dce179a6ded08a942229
maemeier <maemeier@live.com>
2019-12-20 06:58:58 +0100

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FaBars, FaCartPlus } from "react-icons/fa";
 import styled from "styled-components";
 import { ProductConsumer } from "../context";
-import logo from "../images/logo.svg";
+import logo from "../images/logo.jpeg";
 
 const Navbar = () => {
   return (
@@ -11,13 +11,15 @@ const Navbar = () => {
         const { cartItems, handleSidebar, handleCartOpen } = value;
         return (
           <NavWrapper>
-            <div className="nav-center"></div>
-            <FaBars className="nav-icon" onClick={handleSidebar} />
-            <img src={logo} alt="justincase" />
-            <div className="nav-cart">
-              <FaCartPlus className="nav-icon" onClick={handleCartOpen} />
+            <div className="nav-center">
+              <FaBars className="nav-icon" onClick={handleSidebar} />
+              <img src={logo} alt="justincase" />
+              <div className="nav-cart">
+                <FaCartPlus className="nav-icon" onClick={handleCartOpen} />
+
+                <div className="cart-items">{cartItems}</div>
+              </div>
             </div>
-            <div className="cart-items">{cartItems}</div>
           </NavWrapper>
         );
       }}
@@ -25,6 +27,40 @@ const Navbar = () => {
   );
 };
 
-const NavWrapper = styled.nav``;
+const NavWrapper = styled.nav`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  padding: 1rem 1.5rem;
+  background: var(--mainGrey);
+  border-bottom: 3px solid var(--primaryColor);
+  .nav-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+  div.nav-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+  div.nav-cart {
+    position: relative;
+  }
+  .cart-items {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    position: absolute;
+    top: -8px;
+    right: -8px;
+    padding: 0 5px;
+    border-radius: 50%;
+  }
+  img {
+    width: 200px;
+  }
+`;
 
 export default Navbar;
