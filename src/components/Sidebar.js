85b4475b7d29180604e055098404c6fe74f0f517
maemeier <maemeier@live.com>
2019-12-20 22:42:49 +0100

    sidebar toggle

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,7 +9,7 @@ const SideBar = () => {
       {value => {
         const { links, sidebarOpen, handleSidebar } = value;
         return (
-          <SideWrapper>
+          <SideWrapper show={sidebarOpen}>
             <ul>
               {links.map(link => {
                 return (
@@ -32,5 +32,39 @@ const SideBar = () => {
   );
 };
 
-const SideWrapper = styled.nav``;
+const SideWrapper = styled.nav`
+  position: fixed;
+  top: 61px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGrey);
+  z-index: 1;
+  border-right: 4px solid var(--primaryColor);
+  transition: var(--mainTransition);
+  transform: ${props => (props.show ? "translateX(0)" : "translateX(-100%)")};
+  ul {
+    list-style-type: none;
+    padding: 0 !important;
+  }
+
+  .sidebar-link {
+    display: block;
+    font-size: 1.5rem;
+    text-transform: capitalize;
+    color: var(--mainBlack);
+    padding: 0.5rem 1.5rem;
+    background: transparent;
+    transition: var(--mainTransition);
+  }
+  .sidebar-link:hover {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    padding: 0.5rem 1.5rem 0.5rem 2.5rem;
+    text-decoration: none;
+  }
+  @media (min-width: 576px) {
+    width: 20rem;
+  }
+`;
 export default SideBar;
