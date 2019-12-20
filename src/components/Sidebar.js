a62657413809a01268f5e48663cda18165c9ff63
maemeier <maemeier@live.com>
2019-12-20 20:59:29 +0100

    create links in context

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 const SideBar = () => {
-  return <div>SideBar Component</div>;
+  return (
+    <ProductConsumer>
+      {value => {
+        const { links, sidebarOpen, handleSidebar } = value;
+        return (
+          <SideWrapper>
+            <ul>
+              {links.map(link => {
+                return (
+                  <li key={link.id}>
+                    <Link
+                      className="sidebar-link"
+                      to={link.path}
+                      onClick={handleSidebar}
+                    >
+                      {link.text}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SideWrapper>
+        );
+      }}
+    </ProductConsumer>
+  );
 };
 
+const SideWrapper = styled.nav``;
 export default SideBar;
